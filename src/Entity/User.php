17b36f383025557bb89b1d4ff35d8b5041169613
Marine-Sanson <marine_sanson@yahoo.fr>
2024-02-26 18:03:41 +0100

    [#7] configure codacy and symfony insight

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
