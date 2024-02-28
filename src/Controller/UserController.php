3af6e8dd362ff330ebc8d355a0432c2cba232e84
Marine-Sanson <marine_sanson@yahoo.fr>
2024-02-28 10:53:51 +0100

    [#10] Quality changes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use App\Form\RegistrationFormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
@@ -28,15 +29,15 @@ class UserController extends AbstractController
     }
 
     #[Route('/users/create', name: 'user_create')]
-    public function create(Request $request)
+    public function create(Request $request): RedirectResponse 
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $savedUser = $this->userService->register($user, $user->getPassword());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->userService->register($user, $user->getPassword());
 
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
 
@@ -47,13 +48,13 @@ class UserController extends AbstractController
     }
 
     #[Route('/users/{id}/edit', name: 'user_edit')]
-    public function edit(User $user, Request $request)
+    public function edit(User $user, Request $request): RedirectResponse 
     {
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $savedUser = $this->userService->register($user, $user->getPassword());
 
             $this->addFlash('success', "L'utilisateur a bien été modifié");
