183fade9efdb3b2f87abf4e79a1a1e6b4606d969
Marine-Sanson <106590863+Marine-Sanson@users.noreply.github.com>
2024-02-28 11:02:59 +0100

    [#10] User functions (#14)
    * [#10] User functions
    
    * [#10] Quality changes

---
+++ src/Service/UserService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserService
+{
+
+    public function __construct(
+        private readonly UserRepository $userRepository,
+        private readonly UserPasswordHasherInterface $userPasswordHasher
+    ) {}
+    
+    public function getAllUsers(): array
+    {
+        return $this->userRepository->findAll();
+    }
+
+    public function register(User $user, string $password): User
+    {
+
+        $user = (new User())
+            ->setUsername($user->getUsername())
+            ->setEmail($user->getEmail());
+
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                $password
+            )
+        );
+
+        return $this->userRepository->saveUser($user);
+    }
+}
