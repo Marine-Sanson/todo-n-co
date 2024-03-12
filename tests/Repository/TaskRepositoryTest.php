0d073f2dd9f2a0eaa0de25050fbf9793f9b6aeaa
Marine-Sanson <106590863+Marine-Sanson@users.noreply.github.com>
2024-03-12 12:15:59 +0100

    [#19] With first tests (#20)

---
+++ tests/Repository/TaskRepositoryTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Entity\Task;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TaskRepositoryTest extends KernelTestCase
+{
+    private EntityManager $entityManager;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+
+        $this->entityManager = static::$kernel->getContainer()->get('doctrine')
+            ->getManager();
+    }
+
+    public function testSaveTask(): void
+    {
+        // Given
+        $task = (new Task())
+            ->setCreatedAt(new DateTimeImmutable())
+            ->setTitle('task 2 title')
+            ->setContent('task content')
+            ->setIsDone(false);
+
+        // When
+        $this->entityManager->getRepository(Task::class)->saveTask($task);
+
+        // Then
+        $tasks = $this->entityManager->getRepository(Task::class)->findAll();
+        $taskRegistered = $this->entityManager->getRepository(Task::class)->findOneByTitle('task 2 title');
+        $this->assertContains($taskRegistered, $tasks, "This task isn't known");
+    }
+
+    public function testDeleteTask(): void
+    {
+        // Given
+        $task = $this->entityManager->getRepository(Task::class)->findOneByTitle('task 2 title');
+        $id = $task->getId();
+
+        // When
+        $this->entityManager->getRepository(Task::class)->deleteTask($task);
+
+        // Then
+        $tasks = $this->entityManager->getRepository(Task::class)->findAll();
+        $taskDeleted = $this->entityManager->getRepository(Task::class)->findOneById($id);
+        $this->assertNotContains($taskDeleted, $tasks, "This task isn't known");
+    }
+
+    public function testFindAll(): void
+    {
+        // Given
+
+        // When
+        $tasks = $this->entityManager->getRepository(Task::class)->findAll();
+        // Then
+        $this->assertContainsOnlyInstancesOf(Task::class, $tasks);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->entityManager->close();
+    }
+
+}
---
+++ tests/Service/UserServiceTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\User;
+use App\Service\UserService;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+set_exception_handler([new ErrorHandler(), 'handleException']);
+
+class UserServiceTest extends WebTestCase
+{
+
+    private UserService $userService;
+    
+    private EntityManager $entityManager;
+
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+
+        $this->entityManager = static::$kernel->getContainer()->get('doctrine')
+            ->getManager();
+
+        $this->userPasswordHasher = static::$kernel->getContainer()->get('security.user_password_hasher');
+
+        $this->userService = new UserService($this->entityManager->getRepository(User::class), $this->userPasswordHasher);
+    }
+
+    public function testGetAllUsers(): void
+    {
+        // Given
+
+        // When
+        $users = $this->userService->getAllUsers();
+
+        // Then
+        $this->assertContainsOnlyInstancesOf(User::class, $users);
+    }
+
+    public function testRegister(): void
+    {
+        // Given
+        $user = (new User())
+            ->setUsername('new username')
+            ->setEmail('newemail@ex.com');
+        $password = 'password';
+
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                $password
+            )
+        );
+
+        // When
+        $this->userService->register($user, $password);
+
+        // Then
+        $users = $this->entityManager->getRepository(User::class)->findAll();
+        $userRegistered = $this->entityManager->getRepository(User::class)->findOneByEmail('newemail@ex.com');
+        $this->assertContains($userRegistered, $users, "This user isn't known");
+    }
+
+    protected function tearDown(): void
+    {
+        $this->entityManager->close();
+    }
+
+}
